2d9f8be77943ed52a7d1fbc56557c5f2a96d7056
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2018-02-11 18:53:11 +0100

    rename artefact id to follow dash separator convention

--- aws-update-lambda.bat
+++ aws-update-lambda.bat
@@ -1,5 +1,5 @@
 aws lambda update-function-code ^
 --function-name HelloLambda ^
---zip-file fileb:///code/deors.demos/cloud/deors.demos.cloud.awslambda/target/deors.demos.cloud.awslambda-1.0-SNAPSHOT.jar ^
+--zip-file fileb:///code/deors.demos/deors-demos-cloud-awslambda/target/deors-demos-cloud-awslambda-1.0-SNAPSHOT.jar ^
 --region eu-west-1 ^
 --publish
---
+++ readme.md
@@ -0,0 +1,3 @@
+# deors-demos-cloud-awslambda
+
+Demonstration of a simple Java project deployed as a function in Amazon Web Services Lambda.
